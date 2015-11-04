91bd01c98d9ae9928ec98eceba1887fe2ec03463
sreenath <madhyastha@yahoo.com>
2015-11-03 22:28:37 -0500

    Solution Added

---
+++ ConsoleTFSApplication/ConsoleTFSApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTFSApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("i am happy with synology");
+
+        }
+    }
+}
